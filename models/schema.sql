7537269f99cf7de4de56a9dd8d6079d289c8dc57
Cuong XPS <nguyenc72694@gmail.com>
2019-10-22 18:56:43 -0500

    added frontpage

--- models/schema.sql
+++ models/schema.sql
@@ -11,12 +11,14 @@ use exampledb;
     
 use exampledb;
     
-CREATE TABLE `accounts`(
-`id` int(11) NOT NULL AUTO_INCREMENT,
- `username` varchar(255) NOT NULL,
- `password` varchar(255) NOT NULL,
- `email` varchar(255) NOT NULL,
- `created_at` datetime NOT NULL,
- `updated_at` datetime NOT NULL,
- PRIMARY KEY (`id`)) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+CREATE TABLE accounts (
+id int(11) NOT NULL AUTO_INCREMENT,
+ first_name VARCHAR(75) NOT NULL,
+ last_name VARCHAR(75) NOT NULL,
+ username varchar(255) NOT NULL,
+ password varchar(255) NOT NULL,
+ email varchar(255) NOT NULL,
+ created_at datetime NOT NULL,
+ updated_at datetime NOT NULL,
+ PRIMARY KEY (id)) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
  
